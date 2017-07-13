96212695dcae66ffcef03d366aa7fd4622b7d49e
shaunoff <shutch@p3i-inc.com>
2017-07-13 16:02:10 -0400

    added GET/ todos:id with testing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +26,21 @@ app.get('/todos', (req,res)=>{
     res.send({todos})
   }, (e) =>{
     res.status(400).send(e)
+  });
+});
+
+app.get('/todos/:id', (req,res)=>{
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+  Todo.findOne({_id: id}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }, (e) =>{
+    res.status(400).send()
   });
 });
 
