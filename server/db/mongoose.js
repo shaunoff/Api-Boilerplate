444c75973a06f2fdcb4d5175b41a7a1c2ca7ab94
shaun hutch <shaunhutch@shauns-Air.home>
2017-07-16 12:44:22 -0400

    first heroku deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api');
+mongoose.connect("mongodb://heroku_v59mr4zh:bt2g0mou13flrrqem65gv5gqan@ds161262.mlab.com:61262/heroku_v59mr4zh" || 'mongodb://localhost:27017/api' );
 
 module.exports = {mongoose};
