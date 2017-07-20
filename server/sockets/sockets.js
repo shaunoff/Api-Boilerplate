ebadd755021fa1e3fa9fd2a2afe15b45ac8723d1
shaun hutch <shaunhutch@shauns-MacBook-Air.local>
2017-07-20 10:02:42 -0400

    auth sockets

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -1,8 +1,29 @@
 const {Book} = require('../models/book');
+const jwt = require('jwt-simple');
+const config = require('../../config');
 
-var sockets = function(io){
-  io.on('connection', (socket)=>{
-    console.log("new user connected")
+var sockets = (io)=>{
+  const namespace = io.of("/auth")
+
+
+  namespace.use((socket,next)=>{
+    let decoded
+    try {
+    decoded = jwt.decode(socket.handshake.query.token, config.secret)
+    if (decoded){
+      console.log(decoded)
+    }
+    }
+    catch(err){
+      return next(new Error("Need to be logged in"))
+    }
+    if (decoded.role == "Employee"){
+      return next()
+    }
+    return next(new Error("Insufficient Privileges"))
+  })
+
+  namespace.on('connection', (socket)=>{
 
     socket.on('getInitialList',()=>{
       Book.find({}).then((todos)=>{
@@ -16,7 +37,7 @@ var sockets = function(io){
     socket.on('addItem', (data)=>{
       const book = new Book(data)
       book.save().then((doc) => {
-        io.emit('itemAdded', doc)
+        namespace.emit('itemAdded', doc)
       }, (e) => {
         res.status(400).send(e);
       });
@@ -24,6 +45,7 @@ var sockets = function(io){
 
 
   })
+
 }
 
 module.exports = sockets
