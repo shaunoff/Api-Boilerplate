1b26cfe51169b295affb29de4cc1f55f3bf6ca3b
Shaun Hutch <shaunoff@Shauns-MBP.home>
2017-07-21 17:13:17 -0400

    bamboo first

--- server/controllers/bamboo.js
+++ server/controllers/bamboo.js
@@ -1,7 +1,31 @@
 const {Bamboo} = require('../models/bamboo');
+const nodemailer = require('nodemailer');
 
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: 'shutch@p3i-inc.com',
+        pass: 'Woodbird966'
+    }
+});
+
+let mailOptions = {
+    from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    to: 'shutch@p3i-inc.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+};
 
 exports.test = (req, res, next)=> {
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
   var bamboo = new Bamboo({
     data: req.body
   });
