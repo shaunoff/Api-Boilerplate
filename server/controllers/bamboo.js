65444f40be8d250bec52ce75eba2528824363234
Shaun Hutch <shaunoff@Shauns-MBP.home>
2017-07-23 11:54:52 -0400

    add user test

--- server/controllers/bamboo.js
+++ server/controllers/bamboo.js
@@ -4,27 +4,23 @@ const { mjml2html } =require('mjml')
 var googleAuth = require('google-oauth-jwt')
 const axios = require('axios')
 
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: 'shutch@p3i-inc.com',
+        pass: 'Woodbird966'
+    }
+});
 
-// const welcomeEmail = require('../email/welcomeEmail')
-//
-//
-// let transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true, // secure:true for port 465, secure:false for port 587
-//     auth: {
-//         user: 'shutch@p3i-inc.com',
-//         pass: 'Woodbird966'
-//     }
-// });
-//
-// let mailOptions = {
-//     from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-//     to: 'shutch@p3i-inc.com', // list of receivers
-//     subject: 'Hello ✔', // Subject line
-//     text: 'Hello world ?', // plain text body
-//     html: "gsfhjdfghjdfghjdfghjdfsgjhdfs" // html body
-// };
+let mailOptions = {
+    from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    to: 'shutch@p3i-inc.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: "gsfhjdfghjdfghjdfghjdfsgjhdfs" // html body
+};
 const keyFile = process.env.GOOGLE_KEY ? process.env.GOOGLE_KEY : '../../key.pem'
 
 exports.test = (req, res, next)=> {
@@ -33,7 +29,7 @@ exports.test = (req, res, next)=> {
   email: 'new-directory@appraisal-158816.iam.gserviceaccount.com',
   delegationEmail: "shutch@p3i-inc.com",
   // use the PEM file we generated from the downloaded key
-  key: keyFile,
+  keyFile: keyFile,
   // specify the scopes you wish to access
   scopes: ['https://www.googleapis.com/auth/admin.directory.user']
 }, function (err, token) {
@@ -41,42 +37,44 @@ exports.test = (req, res, next)=> {
     console.log(err)
   }
   if(token){
-    console.log("token recieved")
-    axios({
-      method:'get',
-      url:'https://www.googleapis.com/admin/directory/v1/users?domain=p3i-inc.com&query=orgUnitPath:/Employees&maxResults=500',
-      headers: {Authorization: `Bearer ${token}`}
-    })
-    .then(function(response) {
-      console.log(response.data.kind)
-      const bamboo = new Bamboo({
-        data: req.body
-      });
-      bamboo.save().then((doc) => {
-        // transporter.sendMail(mailOptions, (error, info) => {
-        //   if (error) {
-        //       return console.log(error);
-        //   }
-        //   res.send(doc);
-        // });
-        console.log("document recieved")
-
-      }, (e) => {
-        console.log(e);
-      });
-    });
+    console.log(req.body)
+    // axios({
+    //   method:'post',
+    //   //url:'https://www.googleapis.com/admin/directory/v1/users?domain=p3i-inc.com&query=orgUnitPath:/Employees&maxResults=500',
+    //   url:'https://www.googleapis.com/admin/directory/v1/users',
+    //   data:{
+    //     name: {
+    //       familyName: "Fintstone",
+    //       givenName: "Fred"
+    //     },
+    //     password: 'p3ipassword',
+    //     primaryEmail: 'fflintsone@p3i-inc.com',
+    //   },
+    //   headers: {Authorization: `Bearer ${token}`}
+    // })
+    // .then(function(response) {
+    //   const bamboo = new Bamboo({
+    //     data: response.data
+    //   });
+    //   bamboo.save().then((doc) => {
+    //     transporter.sendMail(mailOptions, (error, info) => {
+    //       if (error) {
+    //           return console.log(error);
+    //       }
+    //       res.send(doc);
+    //     });
+    //     console.log("document recieved")
+    //
+    //   }, (e) => {
+    //     console.log(e);
+    //   });
+    // });
   }
 
 
 });
 
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //       return console.log(error);
-  //   }
-  //   console.log('Message %s sent: %s', info.messageId, info.response);
-  // });
-  // console.log(htmlOutput)
+
 
 
 
